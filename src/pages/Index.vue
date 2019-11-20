d5dfab360d91727b2d12e0926e27a6aad7d28eb3
Ben Hong <bencodezen@gmail.com>
2019-11-19 18:44:34 -0700

    feature: Add basic form with new title

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,13 +3,19 @@
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <g-image alt="Example image" src="~/favicon.png" width="135" />
 
-    <h1>The Image</h1>
+    <h1>The Vue Point</h1>
 
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
-      excepturi labore tempore expedita, et iste tenetur suscipit explicabo!
-      Dolores, aperiam non officia eos quod asperiores
-    </p>
+    <form name="contact" method="POST" data-netlify="true">
+      <p>
+        <label>Your Name: <input type="text" name="name"/></label>
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email" required/></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
 
     <p class="home-links">
       <a href="https://gridsome.org/docs/" target="_blank" rel="noopener"
