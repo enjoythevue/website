eea753886432fac9abcdc6167be328b1ceeb0c61
Elizabeth Fine <elizabeth@fine.com>
2019-12-07 19:12:32 -0800

    Adding the podcast description.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -40,13 +40,10 @@ export default {
   data() {
     return {
       pageTitle: 'Enjoy the Vue',
-      podcastDescription: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-           nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor 
-           in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-           pariatur. Excepteur sint occaecat cupidatat non proident, sunt 
-           in culpa qui officia deserunt mollit anim id est laborum.`,
+      podcastDescription: `A new Vue.js podcast bringing you 
+      panel discussions, guest interviews, and much more to
+      keep you up to date on what's happening in the Vue and tech 
+      communities.`,
       panelists: [
         {
           name: 'Chris Fritz',
