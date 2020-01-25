2ec02d737b33cecd8daa04cb9e4b9b0e8f4b345c
Elizabeth Fine <elizabeth@fine.com>
2020-01-24 21:10:57 -0800

    First attempt at setting up Netlify CMS

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,36 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
-}
+  siteName: "Enjoy the Vue",
+  transformers: {
+    remark: {
+      externalLinksTarget: "_blank",
+      externalLinksRel: ["nofollow", "noopener", "noreferrer"],
+      anchorClassName: "icon icon-link",
+      plugins: [
+        // ...global plugins
+      ]
+    }
+  },
+
+  plugins: [
+    {
+      use: "@gridsome/source-filesystem",
+      options: {
+        path: "posts/**/*.md",
+        typeName: "Post",
+        remark: {
+          plugins: [
+            // ...local plugins
+          ]
+        }
+      }
+    },
+    {
+      use: `gridsome-plugin-netlify-cms`,
+      options: {
+        publicPath: `/admin`
+      }
+    }
+  ]
+};
