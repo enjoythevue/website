44a5d9700970cbe6553ad6f8069b6eb71be92156
Ben Hong <bencodezen@gmail.com>
2020-02-03 00:58:25 -0500

    refactor (#92): extract mailerlite login form and style to be consistent with site

--- src/components/EmailSignupForm.vue
+++ src/components/EmailSignupForm.vue
@@ -1,45 +1,52 @@
 <template>
-	<form
-		name="contact"
-		method="POST"
-		class="form"
-		data-netlify="true"
-		data-netlify-honeypot="bot-field" >
-		<input
-		type="hidden"
-		name="form-name"
-		value="contact" />
-		<label class="form__label form__label--half email-signup__label">Your Name:
-			<input
-				type="text"
-				name="name"
-				class="form__input" />
-		</label>
-
-		<label class="form__label form__label--half email-signup__label">Your Email:
-			<input
-				type="email"
-				name="email"
-				class="form__input"
-				required />
-		</label>
-
-		<button
-			class="btn btn--primary email-signup__button"
-			type="submit">Send
-		</button>
-	</form>
+	<div class="ml-form-embed email-signup"
+		data-account="1853384:u6y8f1w6i9"
+		data-form="1753778:g2u3f1">
+	</div>
 </template>
 
 <script>
+import MailerLite from '../scripts/mailerlite';
+
 export default {
 	name: 'EmailSignupForm',
+	created() {
+    MailerLite(
+      window,
+      document,
+      'script',
+      'https://static.mailerlite.com/js/universal.js',
+      'ml'
+    )
+
+    var ml_account = ml('accounts', '1853384', 'u6y8f1w6i9', 'load')
+  }
 }
 </script>
 
 <style lang="scss">
 @import '../styles/forms.scss';
 @import '../styles/buttons.scss';
+
+// Oh it hurts me so much to write CSS like this... :cries:
+#mlb2-1753778 {
+	* {
+		font-family: 'Fira Code', monospace !important;
+	}
+
+	&.ml-form-embedContainer .ml-form-embedWrapper .ml-form-embedBody {
+		padding: 0 !important;
+
+		.ml-form-checkboxRow label p:last-child {
+			font-family: 'Fira Code', monospace !important;
+			font-size: 1.2rem !important;
+		}
+
+		.ml-form-embedSubmit button:first-child {
+			font-family: 'Fira Code', monospace !important;
+		}
+	}
+}
 
 .email-signup {
 	&__button {
