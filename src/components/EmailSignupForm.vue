d31c69892149549b5d8c8a391b287bea8f10bf90
Ben Hong <bencodezen@gmail.com>
2020-02-03 01:00:26 -0500

    style (#92): swap indents to spaces

--- src/components/EmailSignupForm.vue
+++ src/components/EmailSignupForm.vue
@@ -1,16 +1,16 @@
 <template>
-	<div class="ml-form-embed email-signup"
-		data-account="1853384:u6y8f1w6i9"
-		data-form="1753778:g2u3f1">
-	</div>
+  <div class="ml-form-embed email-signup"
+    data-account="1853384:u6y8f1w6i9"
+    data-form="1753778:g2u3f1">
+  </div>
 </template>
 
 <script>
 import MailerLite from '../scripts/mailerlite';
 
 export default {
-	name: 'EmailSignupForm',
-	created() {
+  name: 'EmailSignupForm',
+  created() {
     MailerLite(
       window,
       document,
@@ -30,30 +30,30 @@ export default {
 
 // Oh it hurts me so much to write CSS like this... :cries:
 #mlb2-1753778 {
-	* {
-		font-family: 'Fira Code', monospace !important;
-	}
+  * {
+    font-family: 'Fira Code', monospace !important;
+  }
 
-	&.ml-form-embedContainer .ml-form-embedWrapper .ml-form-embedBody {
-		padding: 0 !important;
+  &.ml-form-embedContainer .ml-form-embedWrapper .ml-form-embedBody {
+    padding: 0 !important;
 
-		.ml-form-checkboxRow label p:last-child {
-			font-family: 'Fira Code', monospace !important;
-			font-size: 1.2rem !important;
-		}
+    .ml-form-checkboxRow label p:last-child {
+      font-family: 'Fira Code', monospace !important;
+      font-size: 1.2rem !important;
+    }
 
-		.ml-form-embedSubmit button:first-child {
-			font-family: 'Fira Code', monospace !important;
-		}
-	}
+    .ml-form-embedSubmit button:first-child {
+      font-family: 'Fira Code', monospace !important;
+    }
+  }
 }
 
 .email-signup {
-	&__button {
-		margin-top: 2rem;
-		
+  &__button {
+    margin-top: 2rem;
+    
     @media (min-width: $breakpoint-sm) {
-			margin: 2rem 0px auto auto;
+      margin: 2rem 0px auto auto;
     }
   }
 }
