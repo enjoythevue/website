e2fae532eaafa108db9cad4fd98ea509dfb3b640
Ben Hong <ben@bencodezen.io>
2020-02-06 10:04:09 -0500

    feature (#106): add basic styling to episode list

---
+++ src/components/EpisodePreview.vue
@@ -0,0 +1,54 @@
+<script>
+export default {
+  name: 'EpisodePreview',
+  props: {
+    episode: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+  <section :class="$style['episode-preview']">
+    <p :class="$style['episode-meta']">
+      #{{ episode.episode_number }} - {{ episode.date_published }}
+    </p>
+    <g-link
+      :class="$style['episode-link']"
+      :to="`/episodes/${episode.episode_number}`"
+    >
+      <h3 :class="$style['episode-title']">{{ episode.episode_title }}</h3>
+    </g-link>
+  </section>
+</template>
+
+<style lang="scss" module>
+.episode {
+  &-link {
+    color: white;
+    text-decoration: none;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+  &-meta {
+    margin-bottom: 1rem;
+  }
+
+  &-preview {
+    border: 1px solid white;
+    padding: 2rem;
+    font-size: 1.5rem;
+    margin-bottom: 2rem;
+  }
+
+  &-title {
+    font-size: 2.5rem;
+    margin-top: 0;
+  }
+}
+</style>
--- src/pages/Episodes.vue
+++ src/pages/Episodes.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout>
-    <main class="episode">
+    <main class="episode" style="margin-top: 0;">
       <section class="episode__section episode__header">
         <div class="episode__section-inner">
           <h1>Episodes</h1>
@@ -10,14 +10,13 @@
       <div class="episode__section">
         <div class="episode__section-inner">
           <h2>Most Recent</h2>
-          <ul>
+          <ul class="episode-list">
             <li
               v-for="episode in $page.allEpisode.edges"
               :key="`episode-${episode.node.episode_number}-list-item`"
+              class="episode-list-item"
             >
-              {{ episode.node.date_published }} <br />
-              Episode #{{ episode.node.episode_number }}:
-              {{ episode.node.episode_title }}
+              <episode-preview :episode="episode.node" />
             </li>
           </ul>
         </div>
@@ -42,13 +41,28 @@ query {
 </page-query>
 
 <script>
+import EpisodePreview from '../components/EpisodePreview'
+
 export default {
   metaInfo: {
     title: 'Episodes'
+  },
+  components: {
+    EpisodePreview
   }
 }
 </script>
 
 <style lang="scss">
 @import '../styles/layout.scss';
+
+.episode-list {
+  padding: 0;
+
+  &-item {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+}
 </style>
