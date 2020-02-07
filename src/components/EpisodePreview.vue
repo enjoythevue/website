87aadd044b17c421122a8a334d5c75516d7182ed
Ben Hong <ben@bencodezen.io>
2020-02-07 07:17:24 -0500

    feature (#106): add episodes manifest page (#109)
    * feature (#106): add episodes page with basic query
    
    * feature (#106): start layout work for episodes
    
    * feature (#106): add basic styling to episode list
    
    * feature (#106): add ability to format dates
    
    * refactor (#106): genericize layout styles
    
    * fix (#106): extra space appearing on episode pages
    
    * refactor (#106): rearrange blocks for more easier scroll flow for coding

---
+++ src/components/EpisodePreview.vue
@@ -0,0 +1,63 @@
+<script>
+import { format } from 'date-fns'
+
+export default {
+  name: 'EpisodePreview',
+  props: {
+    episode: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    publishedDate() {
+      // Utilize date-fns formatting
+      // https://date-fns.org/v2.9.0/docs/format
+      return format(new Date(this.episode.date_published), 'PPPP')
+    }
+  }
+}
+</script>
+
+<template>
+  <section :class="$style['episode-preview']">
+    <p :class="$style['episode-meta']">
+      #{{ episode.episode_number }} - {{ publishedDate }}
+    </p>
+    <g-link
+      :class="$style['episode-link']"
+      :to="`/episodes/${episode.episode_number}`"
+    >
+      <h3 :class="$style['episode-title']">{{ episode.episode_title }}</h3>
+    </g-link>
+  </section>
+</template>
+
+<style lang="scss" module>
+.episode {
+  &-link {
+    color: white;
+    text-decoration: none;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+  &-meta {
+    margin-bottom: 1rem;
+  }
+
+  &-preview {
+    border: 1px solid white;
+    padding: 2rem;
+    font-size: 1.5rem;
+    margin-bottom: 2rem;
+  }
+
+  &-title {
+    font-size: 2.5rem;
+    margin-top: 0;
+  }
+}
+</style>
---
+++ src/pages/Episodes.vue
@@ -0,0 +1,72 @@
+<script>
+import EpisodePreview from '../components/EpisodePreview'
+
+export default {
+  metaInfo: {
+    title: 'Episodes'
+  },
+  components: {
+    EpisodePreview
+  }
+}
+</script>
+
+<template>
+  <Layout>
+    <main class="container episodes">
+      <section class="container__section container__header">
+        <div class="container__section-inner">
+          <h1>Episodes</h1>
+        </div>
+      </section>
+
+      <div class="container__section">
+        <div class="container__section-inner">
+          <h2>Most Recent</h2>
+          <ul class="episode-list">
+            <li
+              v-for="episode in $page.allEpisode.edges"
+              :key="`episode-${episode.node.episode_number}-list-item`"
+              class="episode-list-item"
+            >
+              <episode-preview :episode="episode.node" />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </main>
+  </Layout>
+</template>
+
+<style lang="scss">
+@import '../styles/layout.scss';
+
+.episode-list {
+  padding: 0;
+
+  &-item {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+}
+
+.episodes {
+  color: white;
+}
+</style>
+
+<page-query>
+query {
+  allEpisode {
+    edges {
+      node {
+        date_published
+        episode_number
+        episode_title
+        excerpt
+      }
+    }
+  }
+}
+</page-query>
