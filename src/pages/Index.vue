50d17b41779b8c1f1755a76705b21663d878ca0a
Ben Hong <ben@bencodezen.io>
2020-02-17 11:52:22 -0500

    content: update recent episode

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -61,7 +61,7 @@ export default {
           <a href="https://feeds.fireside.fm/enjoy-the-vue/rss" class="btn btn--primary mb-2r"><rss-logo class="mr-05r" /> Subscribe to our podcast</a>
           <g-link 
             class="btn btn--secondary"
-            to="/episodes/3"
+            to="/episodes/4"
           >
             Listen to the most recent episode
           </g-link>
